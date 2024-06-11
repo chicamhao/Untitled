f1f9251f40481cce9a7e46e0c7655026ac6be247
chicamhao <bluevampire0103@gmail.com>
2024-06-11 15:31:03 +0700

    add sandbox navigation

---
+++ Battle/Assets/Mover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public sealed class Mover : MonoBehaviour
+{
+    [SerializeField] GameObject _target;
+    [SerializeField] NavMeshAgent _navMeshAgent;
+
+    private void Update()
+    {
+        _navMeshAgent.SetDestination(_target.transform.position);
+    }
+}
