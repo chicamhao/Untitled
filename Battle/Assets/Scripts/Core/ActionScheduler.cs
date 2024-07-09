8411725d5a8ab4e7912f7c8994cb0ced40bad6ed
chicamhao <bluevampire0103@gmail.com>
2024-07-09 22:55:07 +0700

    Add basic character behaviors: move, attack, die.

---
+++ Battle/Assets/Scripts/Core/ActionScheduler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Apps.RealTime.Core
+{
+	public interface IAction
+	{
+		void Cancel();
+	}
+
+	public sealed class ActionScheduler : MonoBehaviour
+	{
+		IAction _currentAction;
+
+		/// <summary>
+		/// allow overriding of actions 
+		/// /// </summary>
+		/// <param name="action"></param>
+		public void StartAction(IAction action)
+		{
+			// TODO priority judgment?
+			if (_currentAction == action) return;
+            if (_currentAction != null)
+			{
+				_currentAction.Cancel();
+			}
+			_currentAction = action;
+		}
+	}
+}
+
---
+++ Battle/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,41 @@
+﻿using Apps.RealTime.Core;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Apps.RealTime.Movement
+{
+    public sealed class Mover : MonoBehaviour, IAction
+    {
+        [SerializeField] ActionScheduler _actionScheduler;
+        [SerializeField] GameObject _target;
+        [SerializeField] NavMeshAgent _navMeshAgent;
+        [SerializeField] Animator _animator;
+
+        private readonly static int s_forwardSpeedAnimation = Animator.StringToHash("_forwardSpeed");
+
+        private void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            _navMeshAgent.SetDestination(destination);
+            _navMeshAgent.isStopped = false;
+        }
+
+        public void Cancel()
+        {
+            _navMeshAgent.isStopped = true;
+        }
+
+        private void UpdateAnimator()
+        {
+            // transform velocity direction from global space to local space
+            var velocity = transform.InverseTransformDirection(_navMeshAgent.velocity);
+
+            // forward direction
+            _animator.SetFloat(s_forwardSpeedAnimation, Mathf.Abs(velocity.z));
+        }
+    }
+}
